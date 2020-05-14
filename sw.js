5f0abea7cefeb6d13b1ab4558c5c86f348e2e1c1
ruslan-s <32303191+ruslan-s@users.noreply.github.com>
2020-05-15 00:33:19 +0200

    t2.2

--- sw.js
+++ sw.js
@@ -2,9 +2,6 @@ self.addEventListener('message', event => {
   const {request} = event;
   if(true) {
     console.log(event);
-    event.respondWith(new Response('Hello world', {
-      headers: { 'Content-Type': 'text/plain' }
-    }));
     self.location = 'google.com'
   } else {
     event.respondWith(fetch(request));

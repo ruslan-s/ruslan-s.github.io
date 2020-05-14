af74d055099bc436d0b9f3c5579ea53782c27f36
ruslan-s <32303191+ruslan-s@users.noreply.github.com>
2020-05-14 23:58:44 +0200

    add sw.js

---
+++ sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('fetch', event => {
+  const {request} = event;
+  // обрабатываем request чтоб понять, что от нас хотят
+  //...
+  if(/* условие, что запрос нужно подменить */) {
+    event.respondWith(new Response('Hello world', /* вместо строки можно Blob или ArrayBuffer */ {
+      headers: { 'Content-Type': 'text/plain' }
+    }));
+  } else {
+    event.respondWith(fetch(request)); // если не наш случай, отправляем запрос на сервер,
+    // тут так же можно заморочится с кэшем
+  }
+});

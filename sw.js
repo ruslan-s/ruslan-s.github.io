d16c86339bca1cadb2b06400c0085de3323b406b
ruslan-s <32303191+ruslan-s@users.noreply.github.com>
2020-05-15 00:28:48 +0200

    t2

--- sw.js
+++ sw.js
@@ -1,14 +1,15 @@
-self.addEventListener('fetch', event => {
+self.addEventListener('message', event => {
   const {request} = event;
-  // обрабатываем request чтоб понять, что от нас хотят
-  //...
   if(true) {
     console.log(event);
-    event.respondWith(new Response('Hello world', /* вместо строки можно Blob или ArrayBuffer */ {
+    event.respondWith(new Response('Hello world', {
       headers: { 'Content-Type': 'text/plain' }
     }));
   } else {
-    event.respondWith(fetch(request)); // если не наш случай, отправляем запрос на сервер,
-    // тут так же можно заморочится с кэшем
+    event.respondWith(fetch(request));
   }
 });
+
+self.addEventListner('click', event => {
+  console.log(event);
+})
